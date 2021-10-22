134720f763e6be3606af0afec10f467998135609
Maciej Caderek <maciej.caderek@gmail.com>
2021-10-22 17:52:37 +0200

    Used common-tags instead of prettier to format generated HTML

--- src/internal/prepareFileContent.ts
+++ src/internal/prepareFileContent.ts
@@ -1,6 +1,7 @@
 import { method, multi } from '@arrows/multimethod'
 import { parse } from 'json2csv'
 import { CaseResultWithDiff, SaveOptions, Summary } from './common-types'
+import { stripIndent, html } from 'common-tags'
 
 const flattenResults = (results: CaseResultWithDiff[]) => {
   return results.map((result) => ({
@@ -64,40 +65,41 @@ const prepareHTMLTable = (summary: Summary, options: SaveOptions) => {
 
   const rows = results
     .map((result) => {
-      return `<tr>
+      return stripIndent`
+      <tr>
         ${Object.values(result)
-          .map((item) => `<td>${item}</td>`)
+          .map((item) => html`<td>${item}</td>`)
           .join('')}
-      </tr>`
+      </tr>
+      `
     })
     .join('')
 
-  return `
-      <!DOCTYPE html>
-      <html lang="en">
-        <head>
-          <meta charset="UTF-8" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0"
-          />
-          <meta http-equiv="X-UA-Compatible" />
-          <title>${summary.name}</title>
-        </head>
-        <body>
-          <table>
-            <thead>
-              <tr>
-                ${headers}
-              </tr>
-            </thead>
-            <tbody>
-              ${rows}
-            </tbody>
-          </table>
-        </body>
-      </html>
-    `
+  const markup = html`
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" />
+        <title>Benny: ${summary.name}</title>
+      </head>
+      <body>
+        <table>
+          <thead>
+            <tr>
+              ${headers}
+            </tr>
+          </thead>
+          <tbody>
+            ${rows}
+          </tbody>
+        </table>
+      </body>
+    </html>
+  `
+
+  return stripIndent(markup)
 }
 
 const prepareColors = (percents: number[]) => {
@@ -113,125 +115,126 @@ const prepareHTMLChart = (summary: Summary) => {
   const percents = summary.results.map((result) => result.percentSlower)
   const suffix = summary.date.getTime()
 
-  return `
-      <!DOCTYPE html>
-      <html lang="en">
-        <head>
-          <meta charset="UTF-8" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0"
-          />
-          <meta http-equiv="X-UA-Compatible" />
-          <script src="https://cdn.jsdelivr.net/npm/chart.js@3.5.1/dist/chart.min.js"></script>
-          <title>${summary.name}</title>
-          <style>
-            body {
-              margin: 0;
-              padding: 0;
-              background: #ddd;
-            }
+  const markup = html`
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" />
+        <script src="https://cdn.jsdelivr.net/npm/chart.js@3.5.1/dist/chart.min.js"></script>
+        <title>${summary.name}</title>
+        <style>
+          body {
+            margin: 0;
+            padding: 0;
+            background: #ddd;
+          }
 
-            .container {
-              box-sizing: border-box;
-              height: 96vh;
-              width: 96vw;
-              margin: 2vh 2vw;
-              resize: both;
-              overflow: hidden;
-              padding: 20px;
-              background: white;
-              box-shadow: 0 0 15px #aaa;
-            }
-          </style>
-        </head>
-        <body>
-          <div class="container">
-            <canvas id="chart${suffix}" width="16" height="9"></canvas>
-          </div>
-          <script>
-            const format = (num) => {
-              const [whole, fraction] = String(num).split('.')
-              const chunked = []
-              whole
-                .split('')
-                .reverse()
-                .forEach((char, index) => {
+          .container {
+            box-sizing: border-box;
+            height: 96vh;
+            width: 96vw;
+            margin: 2vh 2vw;
+            resize: both;
+            overflow: hidden;
+            padding: 20px;
+            background: white;
+            box-shadow: 0 0 15px #aaa;
+          }
+        </style>
+      </head>
+      <body>
+        <div class="container">
+          <canvas id="chart${suffix}" width="16" height="9"></canvas>
+        </div>
+        <script>
+          const format = (num) => {
+            const [whole, fraction] = String(num).split('.')
+            const chunked = []
+            whole
+              .split('')
+              .reverse()
+              .forEach((char, index) => {
                 if (index % 3 === 0) {
                   chunked.unshift([char])
                 } else {
                   chunked[0].unshift(char)
                 }
               })
-              
-              const fractionStr = fraction !== undefined ? '.' + fraction : ''
 
-              return chunked.map((chunk) => chunk.join('')).join(' ') + fractionStr
-            }
-            const ctx${suffix} = document
-              .getElementById('chart${suffix}')
-              .getContext('2d')
-            const chart${suffix} = new Chart(ctx${suffix}, {
-              type: 'bar',
-              data: {
-                labels: ${JSON.stringify(labels)},
-                datasets: [
-                  {
-                    data: ${JSON.stringify(values)},
-                    backgroundColor: ${JSON.stringify(prepareColors(percents))},
-                    borderColor: ${JSON.stringify(prepareColors(percents))},
-                    borderWidth: 2
-                  }
-                ]
+            const fractionStr = fraction !== undefined ? '.' + fraction : ''
+
+            return (
+              chunked.map((chunk) => chunk.join('')).join(' ') + fractionStr
+            )
+          }
+          const ctx${suffix} = document
+            .getElementById('chart${suffix}')
+            .getContext('2d')
+          const chart${suffix} = new Chart(ctx${suffix}, {
+            type: 'bar',
+            data: {
+              labels: ${JSON.stringify(labels)},
+              datasets: [
+                {
+                  data: ${JSON.stringify(values)},
+                  backgroundColor: ${JSON.stringify(prepareColors(percents))},
+                  borderColor: ${JSON.stringify(prepareColors(percents))},
+                  borderWidth: 2,
+                },
+              ],
+            },
+            options: {
+              maintainAspectRatio: false,
+              plugins: {
+                title: {
+                  display: true,
+                  text: '${summary.name}',
+                  font: { size: 20 },
+                  padding: 20,
+                },
+                legend: {
+                  display: false,
+                },
+                tooltip: {
+                  callbacks: {
+                    label: (context) => {
+                      return format(context.parsed.y) + ' ops/s'
+                    },
+                  },
+                  displayColors: false,
+                  backgroundColor: '#222222',
+                  padding: 10,
+                  cornerRadius: 5,
+                  intersect: false,
+                },
               },
-              options: {
-                maintainAspectRatio: false,
-                plugins: {
+              scales: {
+                x: {
+                  grid: {
+                    color: '#888888',
+                  },
+                },
+                y: {
                   title: {
                     display: true,
-                    text: '${summary.name}',
-                    font: { size: 20 },
-                    padding: 20
-                  },
-                  legend: {
-                    display: false
-                  },
-                  tooltip: {
-                    callbacks: {
-                      label: (context) => {
-                        return format(context.parsed.y) + ' ops/s'
-                      }
-                    },
-                    displayColors: false,
-                    backgroundColor: '#222222',
+                    text: 'Operations per second',
                     padding: 10,
-                    cornerRadius: 5,
-                    intersect: false
-                  }
-                },
-                scales: {
-                  x: {
-                    grid: {
-                      color: '#888888'
-                    }
                   },
-                  y: {
-                    title: {
-                      display: true,
-                      text: 'Operations per second',
-                      padding: 10
-                    },
-                    grid: {
-                      color: '#888888'
-                    }
-                  }
-                }
-              }
-            })
-          </script>
-        </body>
-      </html>
-    `
+                  grid: {
+                    color: '#888888',
+                  },
+                },
+              },
+            },
+          })
+        </script>
+      </body>
+    </html>
+  `
+
+  return stripIndent(markup)
 }
 
 const prepareFileContent = multi(
